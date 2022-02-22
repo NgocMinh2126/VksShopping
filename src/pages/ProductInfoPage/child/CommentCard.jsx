b4ad4e2154ee852f0f6fc9977d9b3d504406d199
thailnv <64719836+NgocMinh2126@users.noreply.github.com>
2022-02-22 16:15:32 +0700

    update detail info + add comment card

---
+++ src/pages/ProductInfoPage/child/CommentCard.jsx
@@ -0,0 +1,11 @@
+export default function CommentCard(props) {
+
+  return (
+    <div className="comment-card-container flex">
+      <div className="user-info flex">
+
+      </div>
+      <div className="comment-display"></div>
+    </div>
+  );
+}
--- src/pages/ProductInfoPage/child/DetailInfo.jsx
+++ src/pages/ProductInfoPage/child/DetailInfo.jsx
@@ -16,9 +16,28 @@ export default function DetailInfo(props) {
         <div>{createInfo(attributes)}</div>
       </div>
       <div className="tabs">
-        <div className="flex">
-          <h3>Mô tả sản phẩm</h3>
+        <div className="flex tab-title">
+          <h3 className="active">Mô tả sản phẩm</h3>
           <h3>Đánh giá</h3>
+        </div>
+        <div className="description">
+          <p>Bị người yêu lừa dối, Takako bỏ việc và rơi vào chuỗi ngày ngủ triền miên để trốn tránh nỗi buồn. Thế rồi, một cuộc điện thoại từ người cậu ruột cả 10 năm trời không gặp đã đánh thức cô khỏi cơn mộng mị. Takako đồng ý đến trông hiệu sách nửa buổi giúp cậu chỉ để làm vừa lòng mẹ. Nhưng thật ngoài tưởng tượng, chờ đợi cô là hiệu sách Morisaki cũ kỹ với thế giới của hàng nghìn cuốn sách chứa trong mình cả thời gian và lịch sử. Sách đã chữa lành vết thương trong lòng cô.Và hơn thế nữa, Takako tìm thấy bao nhiêu điều mới mẻ và thú vị mà trước đây cô chưa từng biết đến.Câu chuyện nhẹ nhàng mà sâu lắng, đặc biệt với những ai có sở thích sưu tầm sách cổ.</p>
+        </div>
+        <div className="comment">
+          <div className="total flex">
+            <div className="average-rating">5</div>
+            <div className="more">
+              <div className="flex star">
+                <i className="fas fa-star yellow_star" />
+                <i className="fas fa-star yellow_star" />
+                <i className="fas fa-star yellow_star" />
+                <i className="fas fa-star yellow_star" />
+                <i className="fas fa-star yellow_star" />
+              </div>
+              1 nhận xét
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
